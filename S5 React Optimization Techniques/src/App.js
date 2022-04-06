28650deaf5d5cd77c12e6bb038a56ca983fed1db
ilketorun <ilketorun97@gmail.com>
2022-04-06 14:58:04 +0300

    React.memo() reevaluates the component only if one of its props changes

--- S5 React Optimization Techniques/src/App.js
+++ S5 React Optimization Techniques/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useCallback, useState } from "react";
+import DemoOutput from "./components/DemoOutput/DemoOutput";
 
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [paragraphShown, setparagraphShown] = useState(false);
+
+  const clickHandler = useCallback(() => {
+    setparagraphShown((prevParagraphShown) => !prevParagraphShown);
+  }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {/* {paragraphShown && <p>This is paragraph</p>} */}
+      <DemoOutput onShow={paragraphShown} />
+      <DemoOutput onShow={false} />
+      <Button onClick={clickHandler}>Paragraph Toogle</Button>
     </div>
   );
 }
