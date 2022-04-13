6e193304c292cad3e03e233835269d720da40b5c
ilketorun <ilketorun97@gmail.com>
2022-04-13 16:54:04 +0300

    A basic project is imported. Routing is added to the project.

---
+++ S9 React Routing/routing-advanced/src/pages/AllQuotes.js
@@ -0,0 +1,5 @@
+const AllQuotes = () => {
+  return <h1>This is All Quotes page</h1>;
+};
+
+export default AllQuotes;
---
+++ S9 React Routing/routing-advanced/src/pages/NewQuote.js
@@ -0,0 +1,5 @@
+const NewQuote = () => {
+  return <h1>This is New Quote page</h1>;
+};
+
+export default NewQuote;
