bda094f3a2b0e898a65e09393b2ba549bb396455
ilketorun <ilketorun97@gmail.com>
2022-04-15 11:33:54 +0300

    Paths and URLs are optimized. useRouteMatch(), useHistory(), useLocation() hooks are utilized. Built in URLSearchParams() function is used.

--- S9 React Routing/routing-advanced/src/pages/QuoteDetail.js
+++ S9 React Routing/routing-advanced/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -10,7 +10,9 @@ const DUMMY_QUOTES = [
 
 const QuoteDetail = () => {
   const params = useParams();
+  const match = useRouteMatch();
 
+  console.log(match);
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
@@ -20,7 +22,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote author={quote.author} text={quote.text} />
-      <Route path={`/quotes/${params.quoteId}/comments`} exact>
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`} exact>
         <Comments />
       </Route>
     </Fragment>
