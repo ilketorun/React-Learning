5997d07ad157d7b48ef9e0142d1b6c174230a3cf
ilketorun <ilketorun97@gmail.com>
2022-04-16 23:41:09 +0300

    Pages are guarded by checking login info, auto logout added by calculating remaining time.

--- S10 Authentication/intro/src/components/Auth/AuthForm.js
+++ S10 Authentication/intro/src/components/Auth/AuthForm.js
@@ -56,10 +56,15 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        authCtx.login(data.idToken);
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        authCtx.login(data.idToken, expirationTime.toISOString());
         history.replace("/");
       })
-      .catch((err) => alert(err.message));
+      .catch((err) => {
+        alert(err.message);
+      });
   };
 
   return (
