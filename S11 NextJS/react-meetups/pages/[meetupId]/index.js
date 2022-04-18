99791a965ee8ec72789c8d44c0b4f369e34bf782
ilketorun <ilketorun97@gmail.com>
2022-04-18 13:27:25 +0300

    MeetupDetail, Layout, useRouter(),Link
    MeetupDetail component added to keep the page leaner
    Layout added to _app.js to cover all pages
    useRouter() hook's push() function used to make imperative routing
    Link imported from next/link to change path without sending request to server

---
+++ S11 NextJS/react-meetups/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is the description of the first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
