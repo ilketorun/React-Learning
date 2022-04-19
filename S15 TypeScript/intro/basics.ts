ecbf1fc396d8bebb312ca95f7cead764a9223acd
ilketorun <ilketorun97@gmail.com>
2022-04-19 16:39:05 +0300

    Function types and Generics are added

--- S15 TypeScript/intro/basics.ts
+++ S15 TypeScript/intro/basics.ts
@@ -33,3 +33,27 @@ type Human = {
 let person2: Human;
 let people2: Human[];
 
+//Functions and types
+
+//functions also has types
+function returnFunctionType(a:number, b:number): number {
+    return a + b;
+}
+
+// function type void can be given to functions that returns notting
+function printThis(value: any): void {
+    console.log(value)
+} 
+
+//Generics: 
+//This is a feature of typescript. 
+//We can tell it we do not know the type yet 
+//but we know they should be same type.
+
+function insertAtBeginning<T>(array: T [], value: T) {
+
+   const newArray = [value, ...array];
+
+   return newArray;
+
+}
