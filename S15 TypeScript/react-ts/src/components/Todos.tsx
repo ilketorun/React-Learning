8e23a9ee19bba87259aa985e01a660a7ff08bc41
ilketorun <ilketorun97@gmail.com>
2022-04-24 14:27:08 +0300

    Implemented remove todos feature by help of props.

--- S15 TypeScript/react-ts/src/components/Todos.tsx
+++ S15 TypeScript/react-ts/src/components/Todos.tsx
@@ -3,11 +3,17 @@ import Todo from "../Modals/todo";
 import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemove: (id: string) => void }> = (
+  props
+) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem text={item.text} key={item.id} />
+        <TodoItem
+          text={item.text}
+          key={item.id}
+          onRemoveTodo={props.onRemove.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
