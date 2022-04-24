96928931e2943a319ec3172d2899a893d5792e05
ilketorun <ilketorun97@gmail.com>
2022-04-24 15:38:15 +0300

    Quotes are connected to firebase.

--- S9 React Routing/routing-advanced/src/pages/QuoteDetail.js
+++ S9 React Routing/routing-advanced/src/pages/QuoteDetail.js
@@ -1,27 +1,48 @@
-import { Fragment } from "react";
-import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
-import Comments from "../components/comments/Comments";
-import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import { Fragment, useEffect } from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "ilke", text: "This quote is AWESOME" },
-  { id: "q2", author: "ilke Torun", text: "This quote is MORE AWESOME" },
-];
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import Comments from "../components/comments/Comments";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetail = () => {
-  const params = useParams();
   const match = useRouteMatch();
+  const params = useParams();
 
-  console.log(match);
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params;
 
-  if (!quote) {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
-      <HighlightedQuote author={quote.author} text={quote.text} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
@@ -29,7 +50,7 @@ const QuoteDetail = () => {
           </Link>
         </div>
       </Route>
-      <Route path={`${match.path}/comments`} exact>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
